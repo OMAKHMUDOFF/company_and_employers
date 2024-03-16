81285215e66e2f487fa13552c039037ae93b8047
OMAKHMUDOFF <omaxmudovv@gmail.com>
2024-03-16 15:30:25 +0500

    received salary formatting

--- apps/admin.py
+++ apps/admin.py
@@ -3,7 +3,6 @@ from .models import Company, Employer, Employee, Position
 
 # Register your models here.
 
-admin.site.register(Employee)
 admin.site.register(Position)
 
 
@@ -17,7 +16,21 @@ class Companies(admin.ModelAdmin):
 
 @admin.register(Employer)
 class Employers(admin.ModelAdmin):
-    list_display = ("name", "salary", "company", "position")
-    search_fields = ("name", "position", "company")
+    list_display = ("name", "received_salary", "company", "position", "her_employees")
+    search_fields = ("name", "company", "position")
+    list_filter = ("created_at",)
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Employee)
+class Employees(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "salary",
+        "company",
+        "position",
+        "employer",
+    )
+    search_fields = ("name", "company", "position")
     list_filter = ("created_at",)
     readonly_fields = ("created_at", "updated_at")
--- apps/models.py
+++ apps/models.py
@@ -39,11 +39,33 @@ class Employer(models.Model):
     def __str__(self) -> str:
         return self.name
 
+    def her_employees(self):
+        employees = self.employees.all()
+        for employee in employees:
+            return employee
+
+    def received_salary(self):
+        if self.salary < 1000:
+            return self.salary
+        else:
+            salary_count = 0
+            new_salary = ""
+            str_salary = str(self.salary)
+            while 10**salary_count < self.salary:
+                salary_count += 1
+            else:
+                for i in range(salary_count):
+                    new_salary += str(self.salary % 10**i)
+
+            return new_salary
+
 
 class Employee(models.Model):
     name = models.CharField(max_length=255)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    employer = models.ForeignKey(Employer, on_delete=models.DO_NOTHING)
+    employer = models.ForeignKey(
+        Employer, on_delete=models.DO_NOTHING, related_name="employees"
+    )
     position = models.ForeignKey(Position, on_delete=models.DO_NOTHING)
     salary = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
